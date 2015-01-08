131abcd581bed2766e05a8ce6004fed90f74d345
Thomas Stenersen <thomas@Thomass-MacBook-Pro.local>
2015-01-08 23:15:06 +0100

    Cleanup: Moved files to seperate settings folder.

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" "35d23e0bd68243f5ce36c45c64020233349f08549fecb85e93a7272185cdb793" default))))
+ '(custom-safe-themes (quote ("c03d60937e814932cd707a487676875457e0b564a615c1edfd453f23b06fe879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" "35d23e0bd68243f5ce36c45c64020233349f08549fecb85e93a7272185cdb793" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -10,9 +10,12 @@
 (setq site-lisp-dir
       (expand-file-name "site-lisp" user-emacs-directory))
 
+(setq settings-dir
+      (expand-file-name "settings" user-emacs-directory))
+
 ;; Set up load path
-(add-to-list 'load-path user-emacs-directory)
 (add-to-list 'load-path site-lisp-dir)
+(add-to-list 'load-path settings-dir)
 
 ;; Keep emacs Custom-settings in separate file
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
