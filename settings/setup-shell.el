8843e4f10e097c407569b5773f53acf7e63c0110
ths1 <thomas.stenersen@nordicsemi.no>
2016-09-13 10:40:39 +0200

    Windows setup

--- settings/setup-shell.el
+++ settings/setup-shell.el
@@ -5,14 +5,15 @@
 
 ;; bash-completion
 
-(autoload 'bash-completion-dynamic-complete
-  "bash-completion"
-  "BASH completion hook")
-(add-hook 'shell-dynamic-complete-functions
-          'bash-completion-dynamic-complete)
-(add-hook 'shell-command-complete-functions
-          'bash-completion-dynamic-complete)
-
+(when (not (eq system-type 'windows-nt))
+  (autoload 'bash-completion-dynamic-complete
+    "bash-completion"
+    "BASH completion hook")
+  (add-hook 'shell-dynamic-complete-functions
+            'bash-completion-dynamic-complete)
+  (add-hook 'shell-command-complete-functions
+            'bash-completion-dynamic-complete)
+  )
 ;; tab-completion for shell-command
 ;; (require 'shell-command)
 ;; (shell-command-completion-mode)
@@ -40,30 +41,5 @@
 (add-hook 'shell-mode-hook
           (lambda ()
             (define-key shell-mode-map (kbd "C-d") 'comint-delchar-or-eof-or-kill-buffer)))
-
-
-;; (if (eq system-type 'windows-nt)
-;;     ;; Windows // Cygwin config
-;;     (setenv "PATH" (concat "c:/cygwin64/bin;" (getenv "PATH")))
-;;   (setq exec-path (cons "c:/cygwin64/bin/" exec-path))
-;;   (require 'cygwin-mount)
-;;   (cygwin-mount-activate)
-
-;;                                         ;(require 'setup-cygwin)
-
-
-;;   (add-hook 'comint-output-filter-functions
-;;             'shell-strip-ctrl-m nil t)
-;;   (add-hook 'comint-output-filter-functions
-;;             'comint-watch-for-password-prompt nil t)
-;;   (setq explicit-shell-file-name "bash.exe")
-;;   (setq shell-file-name explicit-shell-file-name)
-;;   (setq shell-command-switch "-c")
-;;   )
-
-;; (when (or (eq system-type 'windows-nt) (eq system-type 'msdos))
-;;   (setenv "PATH" (concat "C:\\GNU\\gnuwin32\\bin;" (getenv "PATH")))
-;;   (setq find-program "C:\\GNU\\gnuwin32\\bin\\find.exe"
-;;         grep-program "C:\\GNU\\gnuwin32\\bin\\grep.exe"))
 
 (provide 'setup-shell)
