20a0cae40be9c1be0e8db0566dd37f9c9c2c0d2d
ths1 <thomas.stenersen@nordicsemi.no>
2016-09-13 10:07:07 +0200

    Update custom.el

--- custom.el
+++ custom.el
@@ -4,17 +4,20 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
+   [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector
-   ["#c0c0c0" "#336c6c" "#806080" "#0f2050" "#732f2c" "#23733c" "#6c1f1c" "#232333"])
+   (vector "#cccccc" "#f2777a" "#99cc99" "#ffcc66" "#6699cc" "#cc99cc" "#66cccc" "#515151"))
  '(custom-safe-themes
    (quote
-    ("94ba29363bfb7e06105f68d72b268f85981f7fba2ddef89331660033101eb5e5" "11636897679ca534f0dec6f5e3cb12f28bf217a527755f6b9e744bd240ed47e1" "4f81886421185048bd186fbccc98d95fca9c8b6a401771b7457d81f749f5df75" "9e0b50fcc7f53fc824585f2c60d870bd46e29048eb7ee0fb5f1390437b3d6cdf" "9527feeeec43970b1d725bdc04e97eb2b03b15be982ac50089ad223d3c6f2920" "358978c09c3d65de23e4c4d470ba6d054e9b15222ac61857e98922c50c8f2848" "ef386f7cc2c148fa533d0c0f737cc6e26abec9d7570624ca32d9dd1e2c32100b" "c03d60937e814932cd707a487676875457e0b564a615c1edfd453f23b06fe879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" "35d23e0bd68243f5ce36c45c64020233349f08549fecb85e93a7272185cdb793" default)))
- '(fci-rule-color "#c7c7c7")
+    ("c5d6d43251953cea30a007acf7a6fbca71384558a11fe3d95953ac056a8510dc" "889a93331bc657c0f05a04b8665b78b3c94a12ca76771342cee27d6605abcd0e" "d76ca9b135d95080e395db208acd860d2a883954f2e39b25d6d5cebfb5d7fb90" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "70b9c3d480948a3d007978b29e31d6ab9d7e259105d558c41f8b9532c13219aa" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "b79104a19e95f10698badb711bd4ab25565af3ffcf18fa7d3c7db4de7d759ac8" "94ba29363bfb7e06105f68d72b268f85981f7fba2ddef89331660033101eb5e5" "11636897679ca534f0dec6f5e3cb12f28bf217a527755f6b9e744bd240ed47e1" "4f81886421185048bd186fbccc98d95fca9c8b6a401771b7457d81f749f5df75" "9e0b50fcc7f53fc824585f2c60d870bd46e29048eb7ee0fb5f1390437b3d6cdf" "9527feeeec43970b1d725bdc04e97eb2b03b15be982ac50089ad223d3c6f2920" "358978c09c3d65de23e4c4d470ba6d054e9b15222ac61857e98922c50c8f2848" "ef386f7cc2c148fa533d0c0f737cc6e26abec9d7570624ca32d9dd1e2c32100b" "c03d60937e814932cd707a487676875457e0b564a615c1edfd453f23b06fe879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" "35d23e0bd68243f5ce36c45c64020233349f08549fecb85e93a7272185cdb793" default)))
+ '(fci-rule-color "#515151")
+ '(hl-sexp-background-color "#efebe9")
  '(nrepl-message-colors
    (quote
     ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(nyan-mode t)
  '(org-agenda-restore-windows-after-quit t)
+ '(org-babel-load-languages (quote ((dot . t) (python . t) (emacs-lisp . t))))
  '(org-file-apps
    (quote
     ((auto-mode . emacs)
@@ -22,30 +25,33 @@
      ("\\.x?html?\\'" . "firefox %s")
      ("\\.pdf\\'" . "evince %s")
      ("\\.html\\'" . "firefox %s"))))
+ '(package-selected-packages
+   (quote
+    (dot-mode occidental-theme jabber cmake-ide company-irony-c-headers company-c-headers company-irony exec-path-from-shell company-jedi jedi auto-complete clippy irony ox-asciidoc whitespace-cleanup-mode visual-regexp sr-speedbar smartparens python-mode paredit ox-html5slide ox-gfm org-plus-contrib org-beautify-theme nyan-mode move-text magit leuven-theme jira ido-vertical-mode ido-at-point highlight2clipboard highlight-indentation highlight-escape-sequences helm-swoop helm-projectile helm-gtags guide-key graphviz-dot-mode gitignore-mode gitconfig-mode git-messenger gist ggtags flycheck flx-ido epresent company color-theme-sanityinc-tomorrow cmake-project cmake-mode caroline-theme auctex anzu)))
  '(reftex-default-bibliography (quote ("~/Dropbox/NTNU/master/refs.bib")))
  '(smartparens-global-mode t)
- '(vc-annotate-background "#d4d4d4")
+ '(vc-annotate-background nil)
  '(vc-annotate-color-map
    (quote
-    ((20 . "#437c7c")
-     (40 . "#336c6c")
-     (60 . "#205070")
-     (80 . "#2f4070")
-     (100 . "#1f3060")
-     (120 . "#0f2050")
-     (140 . "#a080a0")
-     (160 . "#806080")
-     (180 . "#704d70")
-     (200 . "#603a60")
-     (220 . "#502750")
-     (240 . "#401440")
-     (260 . "#6c1f1c")
-     (280 . "#935f5c")
-     (300 . "#834744")
-     (320 . "#732f2c")
-     (340 . "#6b400c")
-     (360 . "#23733c"))))
- '(vc-annotate-very-old-color "#23733c"))
+    ((20 . "#f2777a")
+     (40 . "#f99157")
+     (60 . "#ffcc66")
+     (80 . "#99cc99")
+     (100 . "#66cccc")
+     (120 . "#6699cc")
+     (140 . "#cc99cc")
+     (160 . "#f2777a")
+     (180 . "#f99157")
+     (200 . "#ffcc66")
+     (220 . "#99cc99")
+     (240 . "#66cccc")
+     (260 . "#6699cc")
+     (280 . "#cc99cc")
+     (300 . "#f2777a")
+     (320 . "#f99157")
+     (340 . "#ffcc66")
+     (360 . "#99cc99"))))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
