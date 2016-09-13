3fc6013b57c8d546a1af14c3349e06303f8cdc4d
ths1 <thomas.stenersen@nordicsemi.no>
2016-09-13 10:12:36 +0200

    Update c-mode config

--- settings/setup-cc-mode.el
+++ settings/setup-cc-mode.el
@@ -7,15 +7,32 @@
 (define-key c-mode-map  [(control tab)] 'company-complete)
 (define-key c++-mode-map  [(control tab)] 'company-complete)
 
-;; company-c-headers
-(add-to-list 'company-backends 'company-c-headers)
-
 ;; hs-minor-mode for folding source code
 (add-hook 'c-mode-common-hook 'hs-minor-mode)
 (add-hook 'c-mode-common-hook 'fci-mode)
 (add-hook 'c-mode-common-hook 'auto-fill-mode)
-(setenv "SDK_ROOT" "/home/thomsten/devel/SDK9")
+;; (add-hook 'c-mode-common-hook 'irony-mode)
+
+
+;; ;; replace the `completion-at-point' and `complete-symbol' bindings in
+;; ;; irony-mode's buffers by irony-mode's function
+;; (defun my-irony-mode-hook ()
+;;   (define-key irony-mode-map [remap completion-at-point]
+;;     'irony-completion-at-point-async)
+;;   (define-key irony-mode-map [remap complete-symbol]
+;;     'irony-completion-at-point-async))
+;; (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+;; (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+(setq company-clang-arguments ())
+(add-to-list 'company-clang-arguments "-IC:/Users/ths1/devel/MESH/mesh-btle/mesh/include")
+(add-to-list 'company-clang-arguments "-Ic:/Users/ths1/devel/MESH/mesh-btle/mesh/api")
+(add-to-list 'company-clang-arguments "-Ic:/Users/ths1/devel/MESH/mesh-btle/mesh/models/pb_mesh/include")
+(add-to-list 'company-clang-arguments "-Ic:/Users/ths1/devel/SDK/SDK_9.0.0_nrf51/components/device")
+(add-to-list 'company-clang-arguments "-Ic:/Users/ths1/devel/SDK/SDK_9.0.0_nrf51/components/softdevice/s110/headers")
+;; (setq cmake-ide-build-dir "C:/Users/ths1/devel/MESH/mesh-btle/build")
+;; (cmake-ide-setup)
+
 
 (require 'cmake-project)
-
 (provide 'setup-cc-mode)
