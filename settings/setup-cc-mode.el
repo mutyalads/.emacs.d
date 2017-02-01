567c0ebaae0a0b1f6e021aec0e45484bb385b2bd
ths1 <thomas.stenersen@nordicsemi.no>
2017-02-01 12:38:10 +0100

    Fix paths in setup-cc-mode

--- settings/setup-cc-mode.el
+++ settings/setup-cc-mode.el
@@ -9,9 +9,9 @@
 
 ;; hs-minor-mode for folding source code
 (add-hook 'c-mode-common-hook 'hs-minor-mode)
-(setq-default
- whitespace-line-column 100
- whitespace-style       '(face lines-tail))
+;; (setq-default
+;;  whitespace-line-column 100
+;;  whitespace-style       '(face lines-tail))
 (add-hook 'c-mode-common-hook #'whitespace-mode)
 (add-hook 'c-mode-common-hook 'auto-fill-mode)
 ;; (add-hook 'c-mode-common-hook 'irony-mode)
@@ -27,15 +27,15 @@
 ;; (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
 
 (setq company-clang-arguments ())
-(add-to-list 'company-clang-arguments "-IC:/dev/mesh/verification/mod/mbtle/mesh/include/core")
-(add-to-list 'company-clang-arguments "-IC:/dev/mesh/verification/mod/mbtle/mesh/include/dfu")
-(add-to-list 'company-clang-arguments "-IC:/dev/mesh/verification/mod/mbtle/mesh/include/host")
-(add-to-list 'company-clang-arguments "-IC:/dev/mesh/verification/mod/mbtle/mesh/include/prov")
-(add-to-list 'company-clang-arguments "-Ic:/dev/mesh/verification/mod/mbtle/mesh/api")
-(add-to-list 'company-clang-arguments "-Ic:/dev/mesh/verification/mod/mbtle/mesh/models/pb_mesh/include")
-(add-to-list 'company-clang-arguments "-Ic:/dev/mesh/verification/mod/mbtle/lib/softdevice/s130/headers")
-(add-to-list 'company-clang-arguments "-Ic:/dev/mesh/verification/mod/mbtle/mesh/api")
-(add-to-list 'company-clang-arguments "-Ic:/dev/mesh/verification/mod/unity/src")
+(add-to-list 'company-clang-arguments "-I/home/ths1/dev/mbtle/mesh/include/core")
+(add-to-list 'company-clang-arguments "-I/home/ths1/dev/mbtle/mesh/include/dfu")
+(add-to-list 'company-clang-arguments "-I/home/ths1/dev/mbtle/mesh/include/host")
+(add-to-list 'company-clang-arguments "-I/home/ths1/dev/mbtle/mesh/include/prov")
+(add-to-list 'company-clang-arguments "-I/home/ths1/dev/mbtle/mesh/api")
+(add-to-list 'company-clang-arguments "-I/home/ths1/dev/mbtle/mesh/models/pb_mesh/include")
+(add-to-list 'company-clang-arguments "-I/home/ths1/dev/mbtle/lib/softdevice/s130/headers")
+(add-to-list 'company-clang-arguments "-I/home/ths1/dev/mbtle/mesh/api")
+(add-to-list 'company-clang-arguments "-I/home/ths1/dev/cmock/vendor/unity/src")
 ;; (setq cmake-ide-build-dir "C:/Users/ths1/devel/MESH/mesh-btle/build")
 ;; (cmake-ide-setup)
 
